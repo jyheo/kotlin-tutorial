9f60b9553a50ab34b3cb9edde05f6bd4b1afdd88
Junyoung Heo <jyheo0@gmail.com>
2019-09-09 01:12:03 +0900

    kotlin compare example

---
+++ compare.kt
@@ -0,0 +1,16 @@
+data class MyClass(val a: Int, val b: String)
+// data class auto-generates equals/hashCode/toString/copy
+
+fun main() {
+    val str1 = "Hello, Kotlin"
+    val str2 = "Hello, Kotlin"
+    val class1 = MyClass(10, "class1")
+    val class2 = MyClass(10, "class1")
+    val class3 = MyClass(20, "class2")
+
+    println(str1 == str2) // true
+    println(class1 == class2)  // true
+    println(class1 == class3)  // false
+    println(class1 === class2) // false
+}
+
