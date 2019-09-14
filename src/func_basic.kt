d5c9b543571f4ce1127d06f307e199992516e172
Junyoung Heo <jyheo0@gmail.com>
2019-09-14 19:32:24 +0900

    expression body, local function

--- src/func_basic.kt
+++ src/func_basic.kt
@@ -3,9 +3,19 @@ fun myFunc(arg1 : Int, arg2: String = "default", arg3: Int = 10)
     println("$arg1, $arg2, $arg3")
 }
 
+fun sumFunc(a: Int, b: Int) = a + b
+
 fun main()
 {
     myFunc(1, "hello", 5) // 1, hello, 5
     myFunc(arg1 = 2) // 2, default, 10
     myFunc(2, arg3 = 5) // 2, default, 5
+
+    // local function
+    fun localFunc(a: Int) : Int {
+        return a + 10
+    }
+
+    println(sumFunc(1, 2)) // 3
+    println(localFunc(10)) // 20
 }
